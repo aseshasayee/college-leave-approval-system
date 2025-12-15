3c061fbd013e60bc30a5d404aaeda5e6181c933d
Seshasayee <aseshasayee1@gmail.com>
2025-12-15 14:44:11 +0530

    backend framework

---
+++ leave-approval/src/main/java/com/java/leave_approval/LeaveApprovalApplication.java
@@ -0,0 +1,13 @@
+package com.java.leave_approval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeaveApprovalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeaveApprovalApplication.class, args);
+	}
+
+}
